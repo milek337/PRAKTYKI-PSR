06e825daf39e234a25f1c5e2da3625157a7f65c7
milek337 <75125989+milek337@users.noreply.github.com>
2020-12-16 14:09:01 +0100

    Update Main.java

--- praktyki/src/rzutmoneta/Main.java
+++ praktyki/src/rzutmoneta/Main.java
@@ -7,11 +7,11 @@ public class Main {
     }
     public static void rzutmoneta(){
         SecureRandom random = new SecureRandom(); //generator liczb losowych
-        int a = random.nextInt (3); // służy do uzyskania losowej liczby z zakresu od 0 (włącznie) do liczby przekazanej w tym argumencie
-        if(a == 1){ //gdy a jest równe 1 wtedy wypadnie nam orzeł
+        int a = random.nextInt (2); // służy do uzyskania losowej liczby z zakresu od 0 (włącznie) do liczby przekazanej w tym argumencie
+        if(a == 0){ //gdy a jest równe 0 wtedy wypadnie nam orzeł
             System.out.println("Orzeal");
         }
-        else if(a == 2){ // a to gdy a jest równe 2 to wypadnie nam rzeszka
+        else if(a == 1){ // a to gdy a jest równe 2 to wypadnie nam rzeszka
             System.out.println("Reszka");
         }
     }
